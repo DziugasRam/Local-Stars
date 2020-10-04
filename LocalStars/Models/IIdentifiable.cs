4ff772bda008de2cd457bd07cd745389dc750297
DziugasRam <tarimtaram@gmail.com>
2020-10-04 10:39:19 +0300

    Made Identifiable class an interace

---
+++ LocalStars/Models/IIdentifiable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public interface IIdentifiable
+    {
+        public Guid GetId();
+    }
+
+    public class IdentifiableComparer: IEqualityComparer<IIdentifiable>
+    {
+        public bool Equals(IIdentifiable item1, IIdentifiable item2)
+        {
+            return item1.GetId() == item2.GetId();
+        }
+
+        public int GetHashCode(IIdentifiable item)
+        {
+            return item.GetId().GetHashCode();
+        }
+    }
+}
--- LocalStars/Models/Seller.cs
+++ LocalStars/Models/Seller.cs
@@ -2,17 +2,23 @@
 
 namespace Models
 {
-    public class Seller : Identifiable
+    public class Seller : IIdentifiable
     {
-        public Seller(string firstName, string lastName, Guid id, Location location) : base(id)
+        public Seller(string firstName, string lastName, Location location, Guid id)
         {
             FirstName = firstName;
             LastName = lastName;
             Location = location;
+            Id = id;
         }
 
         public string FirstName { get; }
         public string LastName { get; }
         public Location Location { get; }
+        public Guid Id { get; }
+        public Guid GetId()
+        {
+            return Id;
+        }
     }
 }
