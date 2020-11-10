7879eaa617082ffe27c74a2191ff76bd1b8c3334
Simonas Vedeckis <simvedeckis@gmail.com>
2020-11-10 15:50:26 +0200

    Created new react app and added basic routing

---
+++ Frontend/local-stars/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './App.css';
+import Nav from './Nav';
+import Seller from './Seller';
+import Buyer from './Buyer';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Nav/>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/seller" component={Seller}/>
+          <Route path="/buyer" component={Buyer}/>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+)
+
+export default App;
---
+++ Frontend/local-stars/src/Buyer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css';
+
+function Buyer() {
+  return (
+    <div>
+      <h1>Buyer Page</h1>
+    </div>
+  );
+}
+
+export default Buyer;
