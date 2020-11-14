760ddcb85fcbedab8880d43db2756fd7fd2846c7
Simonas Vedeckis <simvedeckis@gmail.com>
2020-11-14 17:04:27 +0200

    Created a card for the products

--- Frontend/local-stars/src/Buyer.tsx
+++ Frontend/local-stars/src/Buyer.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import {Grid} from '@material-ui/core'
+import ProductCard from './ProductCard'
 
 function Buyer() {
   return (
     <div>
-      <h1>Buyer Page</h1>
+      <ProductCard
+      title="Cucumbers from my garden"
+      subtitle="Vegetables"
+      imgSrc="https://www.shethepeople.tv/wp-content/uploads/2019/05/cucumber-e1558166231577.jpg"
+      description="I picked these myself. They're grown with love and without any chemicals"/>
     </div>
   );
 }
