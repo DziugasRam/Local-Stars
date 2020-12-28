076a3432712e94321ea16cb749b25c851dd39f24
GintareLi <71510268+GintareLi@users.noreply.github.com>
2020-12-28 16:19:08 +0200

    Update HomePage.tsx

--- Frontend/local-stars/src/HomePage.tsx
+++ Frontend/local-stars/src/HomePage.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import NavBarHoriz from './Components/NavBarHoriz'
-//import Map from './Components/Map'
+import Map from './Components/Map'
 import ProductCard from './Components/ProductCard'
 import { Grid } from '@material-ui/core';
 import productList from './MockData';
