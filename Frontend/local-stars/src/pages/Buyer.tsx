86d6b8e2bbf7e6663246935b940e5b11fe5ad405
gintare <glideikaite@yahoo.com>
2021-01-04 18:58:42 +0200

    skip and take

--- Frontend/local-stars/src/pages/Buyer.tsx
+++ Frontend/local-stars/src/pages/Buyer.tsx
@@ -1,18 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, constructor } from 'react'
 import ProductCard from '../Components/ProductCard'
 import { Grid } from '@material-ui/core';
 import { serverUrl } from "../configuration";
 import { authFetch } from "../utils/auth";
 import NavBarHoriz from '../Components/NavBarHoriz';
 import BuyerBar from '../Components/BuyerBar';
+import Pagination from "react-js-pagination";
 
 const Buyer = () => {
 
   const [products, setProducts] = useState([]);
+  const[productNumber,setProductNumber]= useState(0);
   const [showLikedProducts, setShowLikedProducts] = useState(false);
+  const [currentPage,setCurrentPage]= useState(1);
 
   useEffect (() => {
-    showAllProducts()
+    authFetch(`${serverUrl}/api/product/count`)
+    .then(resp => resp?.json())
+    .then(data => setProductNumber(data))
+    handlePageChange(currentPage)
   }, [])
 
   const getProductCard = (product: { category: string; description: string; price: string; seller: any; title: string; id: string; image: string;}) => (
@@ -59,19 +65,40 @@ const Buyer = () => {
     setShowLikedProducts(!showLikedProducts)
   }
 
+  const handlePageChange=(pageNumber: any)=> {
+    setCurrentPage(pageNumber);
+    authFetch(`${serverUrl}/api/product/getPage?page=${pageNumber}`)
+    .then(resp => resp?.json())
+    .then(data => setProducts(data))
+  }
+
   return (
     <div>
       <BuyerBar onSearch={onSearch} onSortSelect={onSortSelect} onLiked={onLiked}/>
       <NavBarHoriz onCategoryChange={onCategoryChange}/>
+      <Pagination
+          itemClass="page-item"
+          linkClass="page-link"
+          activePage={currentPage}
+          totalItemsCount={productNumber}
+          onChange={handlePageChange.bind(this)}
+        />
       <Grid container spacing={2}>
       <Grid item xs={1} sm={2}/>
       <Grid item container xs={10} sm={8} spacing={5}>
           {products.map(product => getProductCard(product))}
       </Grid>
-
       <Grid item xs={1} sm={2}/>
-
       </Grid>
+    
+      <Pagination
+          itemClass="page-item"
+          linkClass="page-link"
+          activePage={currentPage}
+          totalItemsCount={productNumber}
+          onChange={handlePageChange.bind(this)}
+        />
+
    </div>
   );
 }
