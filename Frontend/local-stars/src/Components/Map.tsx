c4c4b507c0b35fe2c22a400c0c987f4934763a8b
vilte4 <34207060+vilte4@users.noreply.github.com>
2021-01-07 00:08:19 +0200

    jhmn

--- Frontend/local-stars/src/Components/Map.tsx
+++ Frontend/local-stars/src/Components/Map.tsx
@@ -1,9 +1,9 @@
  import React, {Component} from 'react';
- import GoogleMapReact from "google-map-react";
- import Pin from '../assets/clipart2825061.png'
+ import Pin from '../assets/clipart2825061.png';
+ import GoogleMapReact from 'google-map-react'
 
 
- class Map extends Component {
+class MapsReact extends Component {
 
 
    state = { userLocation: { lat: 32, lng: 32 }, loading: true };
@@ -32,23 +32,34 @@
          return null;
        }
       
-       const Marker = (props:any) => {
-         return <img style={{flex: 1, height: "35px", width: "30px"}} src={Pin} />
-       }
+      const Marker = (props:any) => {
+        return <img style={{flex: 1, height: "35px", width: "30px"}} src={Pin} />
+      }
+
+        const renderMarkers = (map: any , maps: any) => {
+         let marker = new maps.Marker({
+         position: { lat: userLocation.lat, lng: userLocation.lng },
+         map
+         });
+         return marker;
+        }
+        ;
   
-       return (
-       <div style={{height: '150vh', width: '18.5%'}}>
-           <GoogleMapReact
-           bootstrapURLKeys={{ key: "AIzaSyCOlAIs7DJ8Un5CpZ8XFDAE702JGlki9FQ", language: 'en' }}
-           yesIWantToUseGoogleMapApiInternals
-           center={userLocation}
-           defaultZoom={14} >
-             <Marker lat={userLocation.lat} lng={userLocation.lng} />
-           </GoogleMapReact>
-       </div>
-       )
-     }
-   }
+      return (
+      <div style={{height: '100vh', width: '100%'}}>
+          <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyCOlAIs7DJ8Un5CpZ8XFDAE702JGlki9FQ", language: 'en' }}
+          yesIWantToUseGoogleMapApiInternals
+          center={userLocation}
+          defaultZoom={14} 
+          onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps)} >
+          <Marker lat={userLocation.lat} lng={userLocation.lng} />
+          </GoogleMapReact>
+      </div>
+      )
+
+  }
+}
 
 
- export default Map;
+export default MapsReact;
